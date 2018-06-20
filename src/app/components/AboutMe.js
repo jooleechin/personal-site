90f106f88829f5528ff59bc717537e6eae444e47
Joolee Chin <jooleechin@gmail.com>
2018-06-20 16:45:30 -0700

    finished the content of aboutme

--- src/app/components/AboutMe.js
+++ src/app/components/AboutMe.js
@@ -8,21 +8,37 @@ class AboutMe extends Component {
       <div className = "about-box">
         <h1 className="sectionTitle">ABOUT</h1>
         <section className="content">
-          <div className="about-me-blurb">
-            <p>hello! my name is joolee chin and i am a software developer that
-              has full-stack knowledge. I enjoy designing and building responsive
-              websites.</p>
-          </div>
+          <h4 className="blurb">hello! my name is joolee chin and i am a software developer that
+            has full-stack knowledge. I enjoy designing and building responsive
+            websites.
+          </h4>
           <div className="tech">
             <p>programming skills:</p>
             <div class="skills_languages">
               <ul class="language_bubble">
-                <li class="circle">javascript</li>
+                <li class="circle">js ES6</li>
                 <li class="circle">html5</li>
                 <li class="circle">css3</li>
-                <li class="circle sql">mysql</li>
-                <li class="circle node">node.js</li>
-                <li class="circle node">react.js</li>
+                <li class="circle sql">sql</li>
+                <li class="circle sql">python</li>
+              </ul>
+            </div>
+            <div className="skills">
+              <ul>
+                <p>frameworks:</p>
+                <li>// react | react router</li>
+                <li>// node</li>
+                <li>// express.js</li>
+                <li>// knex.js</li>
+              </ul>
+              <ul>
+                <p>tools:</p>
+                <li>// git</li>
+                <li>// github</li>
+                <li>// postgreSQL</li>
+                <li>// heroku</li>
+                <li>// knex.js</li>
+                <li>// postman</li>
               </ul>
             </div>
           </div>
